4419125b0b490997c167d66f19dd2c092c67793c
LaCreArthur <le-tutur@hotmail.fr>
2026-02-15 13:55:13 +0100

    Deploy DogeBread Snake to GitHub Pages

---
+++ client_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const wasm_main: () => void;
+export const __wasm_bindgen_func_elem_4876: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_76568: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_171770: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_5122: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_5119: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_5120: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_171786: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_5118: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_76605: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
